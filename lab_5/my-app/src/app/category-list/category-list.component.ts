a8dedda6ec8b6033ecbfccae20beef012c66f418
Alikhan <a.abikhas@bytepride.dev>
2023-03-01 19:35:41 +0600

    added product filtering

--- lab_5/my-app/src/app/category-list/category-list.component.ts
+++ lab_5/my-app/src/app/category-list/category-list.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-import {categories} from "../categories";
+import {categories, Category} from "../categories";
+import {Product, products} from '../products';
 @Component({
   selector: 'app-category',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent {
+  allProducts = products;
+  products = products;
   categories = categories
+  ngOnInit(){
+
+  }
+  update(category:Category){
+    let index = this.categories.indexOf(category)
+    if (index !== -1) {
+      this.categories[index] = {type: category.type, selected: !category.selected};
+    }
+    this.products = this.allProducts.filter((p) => {
+      let include: boolean = false;
+      this.categories.forEach((c) => {
+        if (c.type == p.categoryType && c.selected) {
+          include = true;
+        }
+      });
+      return include;
+    });
+  }
+
+  share(product: Product) {
+    window.open(`https://t.me/share/url?url=${product.href}&text=${product.name}`);
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
--- lab_5/my-app/src/app/products.ts
+++ lab_5/my-app/src/app/products.ts
@@ -6,11 +6,13 @@ export interface Product {
   image: string;
   href: string;
   description: string;
-  categoryType: string
+  categoryType: string;
+  likes: number;
 }
 
 export const products = [
   {
+    likes: 0,
     id: 1,
     name: 'Ботинки Alexander McQueen',
     categoryType: "Boots",
@@ -21,6 +23,8 @@ export const products = [
     description: 'Трехслойная высокая подошва. Боковые вставки из эластичной ткани. Крупная строчка швов.'
   },
   {
+    likes: 0,
+
     id: 2,
     name: 'Сапоги Alexander McQueen',
     price: 736000,
@@ -31,6 +35,8 @@ export const products = [
     description: 'Верх выполнен из водонепроницаемой эластичной ткани, которая полностью облегает ногу, верхняя часть ребристая. Инновационный каблук из нержавеющей стали с желтым оттенком. Круглый носок.'
   },
   {
+    likes: 0,
+
     id: 3,
     name: 'Isabel Marant',
     categoryType: "Boots",
@@ -41,6 +47,8 @@ export const products = [
     description: "Носок с металлической пряжкой серебристого цвета"
   },
   {
+    likes: 0,
+
     id: 4,
     name: "Ботинки Alexander McQueen",
     price: 539000,
@@ -52,6 +60,8 @@ export const products = [
   },
 
   {
+    likes: 0,
+
     id: 5,
     name: 'Ботильоны Isabel Marant',
     price: 621000,
@@ -61,58 +71,195 @@ export const products = [
     image: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hc5/h26/52093163339806/isabel-marant-2350453-cernyj-105617355-1.jpg",
     description: "Острый носок, каблук трапеция"
   },
+
   {
+    likes: 0,
+
     id: 6,
-    name: "Isabel Marant",
-    price: 634000,
-    categoryType: "Boots",
-    rating: 0,
-    href: "https://kaspi.kz/shop/p/isabel-marant-2352632-chernyi-105617208/?c=750000000",
-    image: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8b/h26/52098453471262/isabel-marant-2352632-cernyj-105617208-1.jpg",
-    description: "Полукруглый носок, квадратный каблук"
+    name: 'Lenovo IdeaPad 3',
+    price: 169990,
+    rating: 4.3,
+    categoryType: "Laptops",
+    href: "https://kaspi.kz/shop/p/lenovo-ideapad-3-15itl6-82h8005grk-serebristyi-108090705/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h92/he5/66993674059806/lenovo-ideapad-3-15itl6-82h8005grk-serebristyi-108090705-1.jpg",
+    description: ""
   },
-
   {
+    likes: 0,
+
     id: 7,
-    name: 'GIORGIO FABIANI',
-    price: 219780,
-    categoryType: "Boots",
-    rating: 0,
-    href: "https://kaspi.kz/shop/p/giorgio-fabiani-f191651-102125268/?c=750000000",
-    image: "https://resources.cdn-kaspi.kz/img/m/p/hb2/h20/64564170817566.jpg?format=preview-medium",
-    description: "Высота каблука: 6 см"
+    name: 'Acer Nitro 5',
+    price: 389980,
+    rating: 4.3,
+    categoryType: "Laptops",
+    href: "https://kaspi.kz/shop/p/acer-nitro-5-an515-57-nh-qeker-004-chernyi-108194028/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h9b/h1b/67236399120414/acer-nitro-5-an515-57-nh-qeker-004-chernyi-108194028-1.jpg",
+    description: ""
   },
   {
+    likes: 0,
+
     id: 8,
-    name: "GIOVANNI FABIANI",
-    price: 239274,
-    rating: 0,
-    categoryType: "Boots",
-    image: "https://resources.cdn-kaspi.kz/img/m/p/h4e/ha9/64149408645150.jpg?format=preview-medium",
-    href: "https://kaspi.kz/shop/p/giovanni-fabiani-g5449ms-102279719/?c=750000000",
-    description: "Сезон: зима"
+    name: 'Apple Macbook Air 13',
+    price: 478888,
+    rating: 4.4,
+    categoryType: "Laptops",
+    href: "https://kaspi.kz/shop/p/apple-macbook-air-13-mgn63-seryi-100797845/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h67/h95/33125682380830/apple-macbook-air-2020-13-3-mgn63-seryj-100797845-1-Container.jpg",
+    description: ""
+  },
+  {
+    likes: 0,
+
+    id: 9,
+    name: 'ASUS TUF Gaming A15',
+    price: 369990,
+    rating: 5.0,
+    categoryType: "Laptops",
+    href: "https://kaspi.kz/shop/p/asus-tuf-gaming-a15-fa506ihrb-hn084-90nr07g7-m008c0-chernyi-106255184/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/medias/sys_master/images/images/ha2/he7/62100002570270/asus-tuf-gaming-a15-fa506ihrb-hn084-90nr07g7-m008c0-cernyj-106255184-1.jpg",
+    description: ""
+  },
+  {
+    likes: 0,
+
+    id: 10,
+    name: 'Apple Macbook Pro 14',
+    price: 980990,
+    rating: 3.7,
+    categoryType: "Laptops",
+    href: "https://kaspi.kz/shop/p/apple-macbook-pro-14-mkgp3-seryi-102866247/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/ha3/h9b/64200744763422.jpg?format=preview-medium",
+    description: ""
   },
 
   {
-    id: 9,
-    name: 'Alexander Mc Queen',
-    price: 382000,
-    categoryType: "Boots",
-    rating: 0,
-    image: "https://resources.cdn-kaspi.kz/img/m/p/hd8/h22/63753526870046.jpg?format=preview-medium",
-    href: "https://kaspi.kz/shop/p/alexander-mc-queen-657575w4l3m-1355-chernyi-103570221/?c=750000000",
-    description: "Модель: ботинки"
+    likes: 0,
+
+    id: 11,
+    name: 'Apple Iphone 13',
+    price: 370990,
+    rating: 4.3,
+    categoryType: "Smartphones",
+    href: "https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h09/ha2/64208868933662.jpg?format=preview-medium",
+    description: ""
+  },
+
+  {
+    likes: 0,
+
+    id: 12,
+    name: 'Xiaomi Redmi 10 Pro 8',
+    price: 128880,
+    rating: 2.5,
+    categoryType: "Smartphones",
+    href: "https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h94/h74/64487156645918.jpg?format=preview-medium",
+    description: ""
+  },
+
+  {
+    likes: 0,
+
+    id: 13,
+    name: 'Samsung Galaxy A33',
+    price: 127880,
+    rating: 4.5,
+    categoryType: "Smartphones",
+    href: "https://kaspi.kz/shop/p/samsung-galaxy-a33-5g-6-gb-128-gb-goluboi-104399147/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/hed/he1/64374718890014.jpg?format=preview-medium",
+    description: ""
+  },
+
+  {
+    likes: 0,
+
+    id: 14,
+    name: 'Vivo Y35',
+    price: 127880,
+    rating: 4.5,
+    categoryType: "Smartphones",
+    href: "https://kaspi.kz/shop/p/vivo-y35-4-gb-128-gb-zolotistyi-107358117/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h3e/hbb/65154223570974.jpg?format=preview-medium",
+    description: ""
+  },
+
+  {
+    likes: 0,
+
+    id: 15,
+    name: 'Samsung Galaxy A32',
+    price: 177760,
+    rating: 3.3,
+    categoryType: "Smartphones",
+    href: "https://kaspi.kz/shop/p/samsung-galaxy-a32-4-gb-64-gb-chernyi-101171611/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h0d/h2a/64008665956382.jpg?format=preview-medium",
+    description: ""
+  },
+
+  {
+    likes: 0,
+
+    id: 16,
+    name: 'MOYVI рюкзак',
+    price: 9500,
+    rating: 3.9,
+    categoryType: "Backpacks",
+    href: "https://kaspi.kz/shop/p/moyyi-rjukzak-6010-chernyi-iz-iskusstvennoi-kozhi-105894061/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h98/h7e/52601961512990/moyyi-rukzak-6010-cernyj-iz-iskusstvennoj-kozi-105894061-1.jpg",
+    description: ""
+  },
+
+  {
+    likes: 0,
+
+    id: 17,
+    name: 'LEGRAN рюкзак',
+    price: 8500,
+    rating: 3.9,
+    categoryType: "Backpacks",
+    href: "https://kaspi.kz/shop/p/legran-rjukzak-102016-bezhevyi-108624872/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h55/hd7/68332053823518/legran-ryukzak-102016-bezhevyi-108624872-1.jpg",
+    description: ""
   },
   {
-    id: 10,
-    name: "Кроссовки Nike ZOOMX VAPORFLY NEXT",
-    price: 202300,
-    rating: 5.0,
-    categoryType: "Boots",
-    href: "https://kaspi.kz/shop/p/nike-zoomx-vaporfly-next-2-dv9428-100-mul-tikolor-107715498/?c=750000000",
-    image: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h89/ha3/66117664636958/nike-dv9428-100-mul-tikolor-107715498-1.jpg",
-    description: "Профессиональные беговые кроссовки с карбоновой пластиной"
-  }
+    likes: 0,
+
+    id: 18,
+    name: 'Рюкзак Наруто Аниме',
+    price: 9500,
+    rating: 3.9,
+    categoryType: "Backpacks",
+    href: "https://kaspi.kz/shop/p/rjukzak-naruto-anime-108294538/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/ha6/h09/67492803674142.jpg?format=preview-medium",
+    description: ""
+  },
+  {
+    likes: 0,
+
+    id: 19,
+    name: 'BAILLR рюкзак',
+    price: 8200,
+    rating: 3.9,
+    categoryType: "Backpacks",
+    href: "https://kaspi.kz/shop/p/baillr-rjukzak-2001-belyi-105460018/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/hb2/he4/65070772191262.jpg?format=preview-medium",
+    description: ""
+  },
+  {
+    likes: 0,
+
+    id: 20,
+    name: 'XIGUL рюкзак',
+    price: 7900,
+    rating: 3.9,
+    categoryType: "Backpacks",
+    href: "https://kaspi.kz/shop/p/xigul-rjukzak-sm0012-chernyi-105822119/?c=750000000",
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h68/hc8/64449712029726.jpg?format=preview-medium",
+    description: ""
+  },
+
 ];
 
 
