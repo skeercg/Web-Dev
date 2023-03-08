80ba569da4017582192fca86105fa2ff287eb7b8
Alikhan <a.abikhas@bytepride.dev>
2023-03-08 18:05:31 +0600

    done

--- lab_6/my-app/src/app/app.module.ts
+++ lab_6/my-app/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { AlbumDetailComponent } from './components/album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './components/album-photos/album-photos.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AlbumsComponent,
+    AlbumDetailComponent,
+    AlbumPhotosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
